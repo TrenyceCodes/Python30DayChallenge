6970e23d9840aadde79a99e7b5bacb603ef57f2f
Neice <52137719+TrenyceCodes@users.noreply.github.com>
2024-09-22 12:02:43 -0400

    Adding first file

---
+++ main.py
@@ -0,0 +1,30 @@
+#basic syntax
+#variables(covered), 
+# string(covered), 
+# floats(covered), 
+# booleans(covered), 
+#functions, 
+# comments(covered), 
+# operators(covered), 
+# indentation(covered), 
+# semicolons, 
+# loop control statements
+
+#python indention
+if 5 > 2:
+    print("hello")
+
+#python comments
+#this is a single line comment 
+# this can be a 
+# multiline comment
+"""
+this is a multi line
+comment
+"""
+\
+import outputTest as ot
+
+ot.output(5*6)
+ot.output(4)
+ot.output(3)
