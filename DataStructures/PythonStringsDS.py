7c6effb83166ceaaf896f412d9cc6ebda869d860
Neice <52137719+TrenyceCodes@users.noreply.github.com>
2024-10-01 13:46:47 -0400

    Practicing Python Strings Data Structures Slicing

---
+++ DataStructures/PythonStringsDS.py
@@ -0,0 +1,8 @@
+#Practice Problem
+message = "Learning Python is fun and rewarding!"
+
+#task 1 Extract the first 8 characters of the string
+print(message[0:8])
+
+#task 2 Extract the last 9 characters of the string.
+print(message[26:37])
--- PythonEnum.py
+++ PythonEnum.py
@@ -1,8 +1,10 @@
 from datetime import date
 from enum import Enum, auto
+from typing import List
+
 
 class Weekdays(Enum):
-    Monday = auto()
+    Monday = auto()  #auto sets the enum number automatically
     Tuesday = auto()
     Wednesday = auto()
     Thursday = auto()
@@ -11,7 +13,7 @@ class Weekdays(Enum):
     Sunday = auto()
     pass
 
-    @classmethod
+    @classmethod  #converts function to a class method
     def current_weekday(weekdayClass, current_date: date):
         try:
             return weekdayClass(current_date.isoweekday())
@@ -19,7 +21,16 @@ class Weekdays(Enum):
             print(f"Error: {error}")
             return error
 
+    @classmethod
+    def show_all_weekdays(weekdayClass) -> List[str]:
+        #for day in Weekdays:
+        #    return weekdayClass(day.name)
+        return [day.name for day in weekdayClass]
+
+
 if __name__ == '__main__':
     current_day: Weekdays = Weekdays.current_weekday(current_date=date.today())
+    days: list[str] = Weekdays.show_all_weekdays()
+    print(f"All weekdays: {days}")
     if current_day:
         print(f"Today is {current_day}")
